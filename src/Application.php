96b8af235973d5075f589246b7f29fa16555797c
Nikhil Joshi <nikhil.joshi.hs@gmail.com>
2019-10-24 16:58:54 +0530

    Update required response structure. Initialize Request and Response object in constructor.

--- src/Application.php
+++ src/Application.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace Blossom\BackendDeveloperTest;
 
 use Symfony\Component\HttpFoundation\Request;
@@ -6,7 +7,7 @@ use Symfony\Component\HttpFoundation\Response;
 
 /**
  * You should implement this class however you want.
- * 
+ *
  * The only requirement is existence of public function `handleRequest()`
  * as this is what is tested. The constructor's signature must not be changed.
  */
@@ -16,12 +17,46 @@ class Application
      * By default the constructor takes a single argument which is a config array.
      *
      * You can handle it however you want.
-     * 
+     *
      * @param array $config Application config.
+     */
+
+    /**
+     * @var $request Symfony\Component\HttpFoundation\Request
+     */
+    private $request;
+
+    /**
+     * @var $response Symfony\Component\HttpFoundation\Response
+     */
+    private $response;
+
+    /**
+     * @var $http_status
+     */
+    private $http_status;
+
+    /**
+     * Application constructor.
+     * @param array $config
      */
     public function __construct(array $config)
     {
+        $this->http_status = Response::HTTP_OK;
+        $this->request = new Request(
+            $_GET,
+            $_POST,
+            [],
+            $_COOKIE,
+            $_FILES,
+            $_SERVER
+        );
 
+        $this->response = new Response(
+            'Content',
+            $this->http_status,
+            ['content-type' => 'application/json']
+        );
     }
 
     /**
@@ -29,13 +64,21 @@ class Application
      *
      * You should implement it however you want and it should return a Response
      * that passes all tests found in EncoderTest.
-     * 
-     * @param  Request $request The request.
+     *
+     * @param Request $request The request.
      *
      * @return Response
      */
     public function handleRequest(Request $request): Response
     {
-
+        $content = [
+            "message" => "",
+            "error" => "",
+            "success" => true,
+            "url" => "string"
+        ];
+        $this->response->setCharset('UTF-8');
+        $this->response->setContent(json_encode($content));
+        return $this->response;
     }
 }
